8d9450570de1d29bdc849790605333f77b322799
Mukesh M <mukeshm1989@gmail.com>
2021-01-08 23:38:17 +0530

    design login

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,8 @@ const state ={
 }
    
 module.exports.connect = async function(done){
-    const url =await "mongodb+srv://mukesh:9km7BWUYUXRquSFD@mymart.au7ts.mongodb.net/mymart?retryWrites=true&w=majority" //"mongodb://localhost:27017"
-    //const url ="mongodb://localhost:27017"
+   // const url =await "mongodb+srv://mukesh:9km7BWUYUXRquSFD@mymart.au7ts.mongodb.net/mymart?retryWrites=true&w=majority" //"mongodb://localhost:27017"
+    const url ="mongodb://localhost:27017"
     const dbname= "mymart"
     mongoClient.connect(url,{useNewUrlParser:true,useUnifiedTopology:true},(err,data)=>{
         // console.log('data---',data);
