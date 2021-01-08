031947067f0e8f9758fe4c914b548091b014f1be
Mukesh M <mukeshm1989@gmail.com>
2021-01-08 16:31:16 +0530

    make it better

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ const mongoClient = require('mongodb').MongoClient
 const state ={
     db:null
 }
-//mongodb+srv://mukesh:<password>@cluster0.au7ts.mongodb.net/<dbname>?retryWrites=true&w=majority
+//mongodb+srv://mukesh:<password>@cluster0.au7ts.mongodb.net/<dbname>?retryWrites=true&w=majority   FfeoIqhFFjSvggq7
 module.exports.connect = function(done){
-    const url ='mongodb+srv://mukesh:mukesh654654@cluster0.au7ts.mongodb.net/mymart?retryWrites=true&w=majority'  //"mongodb://localhost:27017"
+    const url ="mongodb+srv://mukesh:FfeoIqhFFjSvggq7@mymart.au7ts.mongodb.net/mymart?retryWrites=true&w=majority" //"mongodb://localhost:27017"
     const dbname= "mymart"
     mongoClient.connect(url,{useNewUrlParser:true,useUnifiedTopology:true},(err,data)=>{
         if(err){
