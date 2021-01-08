703a17be8c594fccf499c0a4e23a87b8822c1ce1
Mukesh M <mukeshm1989@gmail.com>
2021-01-08 14:51:52 +0530

    dab uodated..

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,13 @@ const mongoClient = require('mongodb').MongoClient
 const state ={
     db:null
 }
-
+//mongodb+srv://mukesh:<password>@cluster0.au7ts.mongodb.net/<dbname>?retryWrites=true&w=majority
 module.exports.connect = function(done){
-    const url ="mongodb://localhost:27017"
+    const url ='mongodb+srv://mukesh:mukesh654654@cluster0.au7ts.mongodb.net/mymart?retryWrites=true&w=majority'  //"mongodb://localhost:27017"
     const dbname= "mymart"
     mongoClient.connect(url,{useNewUrlParser:true,useUnifiedTopology:true},(err,data)=>{
         if(err){
+            console.log(err);
             return done(err)
         }else{
             state.db = data.db(dbname)
